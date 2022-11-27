e74acb61c346e534a02e72ad191755a66eb5fc28
Tatiana Gandra <tatianagandra78@gmail.com>
2022-11-26 21:16:40 -0300

    feat: creat user module

---
+++ src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './services/user.service';
+
+@Module({
+  
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UsersModule {}
