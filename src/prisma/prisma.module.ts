8d266f43494ca68b5d7a1c9f7dec6f5a58131c44
tatiana lima <tatianagandra78@gmail.com>
2022-12-03 13:36:13 -0300

    feat: config prisma

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-import { OrderController } from './order.controller';
 
 @Module({
   providers: [PrismaService],
