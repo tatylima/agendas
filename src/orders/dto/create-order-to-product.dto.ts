2bb9e473c1a73a670253db113e876d912256299e
tatiana lima <tatianagandra78@gmail.com>
2022-12-06 22:26:02 -0300

    feat : create order dto to products

---
+++ src/orders/dto/create-order-to-product.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsPositive, IsUUID } from 'class-validator';
+
+export class CreateOrderToProctDto {
+  @ApiProperty({
+    description: 'id for product',
+    example: '3755c7c0-b6a4-4d46-8590-9970073f6eb6',
+  })
+  @IsUUID()
+  productId: string;
+
+  @ApiProperty({
+    description: 'quantity for product',
+    example: '2',
+  })
+  @IsInt()
+  @IsPositive()
+  quantity: number;
+}
