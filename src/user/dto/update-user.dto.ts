b9786573ad920c18b5b8973c0144358d4c5362da
tatiana lima <tatianagandra78@gmail.com>
2022-12-06 21:43:15 -0300

    feat: correção de erros

--- src/user/dto/update-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType (CreateUserDto) {}
--- src/user/entities/user.entity.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  id?: string;
-  nickname: string;
-  name: string;
-  password: string;
-  image: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
