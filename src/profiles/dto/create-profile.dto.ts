dc21e2d7eacc48077b155446835ed38c9dcdf3b3
tatiana lima <tatianagandra78@gmail.com>
2022-12-08 23:07:53 -0300

    feat: create profile dto

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,3 +1,27 @@
-export class createProfileDto{
-    
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl } from 'class-validator';
+
+export class createProfileDto {
+  @IsString()
+  @ApiProperty({
+    description: 'O nome usado para o perfil',
+    example: 'TatianaGandra123',
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'ID do Usuario(admin)',
+    example: 'f7f45b19-bb7c-4f0d-87ea-f0d3c04423b5',
+  })
+  userId: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do Perfil',
+    example: 'https://avatars.githubusercontent.com/u/97984721?s=400&u=c611aadf5b37eadeb851c125d233ad670af240a4&v=4',
+  })
+  image: string;
+
+  productId?: string;
 }
