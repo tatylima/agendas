cf48f99a5fd4428cc45537d8458a9bd0acea8630
tatiana lima <tatianagandra78@gmail.com>
2022-12-08 23:08:49 -0300

    feat: modification profiles services

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,22 +1,128 @@
-import { Injectable} from '@nestjs/common';
+import { Injectable,NotFoundException,UnauthorizedException} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from 'src/users/entities/user.entity';
+import { handleErrorUnique } from 'src/utils/handle.error.unique';
+import { Profile } from './entities/profile.entity';
 import { createProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
 export class ProfilesService{
-    create(createProfileDto:createProfileDto){
-        return ' Adiciona um novo perfil';
+    constructor(private readonly prisma:PrismaService){}
     }
-    findAll(){
-        return ' Retornar todos os perfis';
+    findAll(user:User){
+        return this.prisma.profile.findMany({
+            where:{
+                userId:id,
+            },
+            include:{
+                user:true,
+                products: true,
+                favorite:{
+                    select:{
+                        products:{
+                            select:{
+                                title:true,
+                            },
+
+                        },
+                    },
+                },
+            },
+        });
     }
-    findOne(id:number){
-        return ' Retorna um id do perfil';
+    async findById(id:string){
+    const record = await this.prisma.profile.findUnique({
+        where:{id:id},
+        include:{
+            products:true,
+        },
+    }),
+    if (!record) {
+        throw new NotFoundException(`Registro com o ID: '${id}' não encontrado`);
+      }
+      return record;
     }
-    update(id:number, UpdateProfileDto:UpdateProfileDto){
-        return ' Retorna atualização de id do perfil';
+  
+    async create(userId: string, dto: CreateProfileDto) {
+      if (dto.gameId) {
+        return await this.prisma.profile
+          .create({
+            data: {
+              name: dto.name,
+              image: dto.image,
+              userId: dto.userId,
+              products: {
+                connect: {
+                  id: dto.gameId,
+                },
+              },
+            },
+            include: {
+              products: true,
+              favorite: true,
+            },
+          })
+          .catch(handleError);
+      } else {
+        return await this.prisma.profile
+          .create({
+            data: {
+              name: dto.name,
+              image: dto.image,
+              userId: dto.userId,
+            },
+            include: {
+              favorite: true,
+            },
+          })
+          .catch(handleError);
+      }
     }
-    delete(id:number){
-        return ' Deletar um perfil';
+  
+    async update(userId: string, id: string, dto: UpdateProfileDto) {
+      await this.findById(id);
+      if (dto.productId) {
+        return this.prisma.profile
+          .update({
+            where: { id },
+            data: {
+              name: dto.name,
+              image: dto.image,
+              userId: userId,
+              favorite: {
+                connect: {
+                  id: dto.productId,
+                },
+              },
+            },
+            include: { favorite: true },
+          })
+          .catch(handleError);
+      } else {
+        return this.prisma.profile
+          .update({
+            where: { id },
+            data: {
+              name: dto.name,
+              image: dto.image,
+              userId: userId,
+            },
+            include: { favorite: true },
+          })
+          .catch(handleError);
+      }
     }
-}
+  
+    async delete(user: User, id: string) {
+      if (user.isAdmin) {
+        await this.findById(id);
+        await this.prisma.profile.delete({ where: { id } });
+      } else {
+        throw new UnauthorizedException(
+          'Usuário não autorizado. Contate o Administrador!',
+        );
+      }
+    }
+  }
+    
